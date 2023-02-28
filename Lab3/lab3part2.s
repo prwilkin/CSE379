b2066319a773d08e493061ad2d3359a370ec4c39
prwilkin <90287120+prwilkin@users.noreply.github.com>
2023-02-28 00:10:09 -0500

    uart_init
    Title code not tested but written

--- Lab3/lab3part2.s
+++ Lab3/lab3part2.s
@@ -74,6 +74,56 @@ output_character:
 uart_init:
 	PUSH {lr}  ; Store register lr on stack
 
+	;Provide clock to UART0
+	MOV r0, #0xE618
+	MOVT r0, #0x400F	;set address to 0x400FE618
+	MOV r1, #0x1		;mark bit #0 as 1
+	STR r1,	[r0]		;store @ address
+	;Enable clock to PortA
+	MOV r0, #0xE608		;set address to 0x400FE618
+	MOV r1, #0x1		;mark bit #0 as 1
+	STR r1, [r0]		;store @ address
+	;Disable UART0 Control
+	MOV r0, #0xC030
+	MOVT r0, #0x4000	;set address to 0x4000C030
+	MOV r1, #0x0
+	STR r1, [r0]		;store @ address
+	;Set UART0_IBRD_R for 115,200 baud
+	MOV r0, #0xC024		;set address to 0x4000C024
+	MOV r1, #0x8
+	STR r1, [r0]		;store @ address
+	;Set UART0_FBRD_R for 115,200 baud
+	MOV r0, #0xC028		;set address to 0x4000C028
+	MOV r1, #0x2C
+	STR r1, [r0]
+	;Use System Clock
+	MOV r0, #0xCFC8		;set address to 0x4000CFC8
+	MOV r1, #0x0
+	STR r1, [r0]
+	;Use 8-bit word length, 1 stop bit, no parity
+	MOV r0, #0xC02C		;set address to 0x4000C02C
+	MOV r1, #0x3C
+	STR r1, [r0]
+	;Enable UART0 Control
+	MOV r0, #0xC030		;set address to 0x4000C030
+	MOV r1, #12D
+	STR r1, [r0]
+	;Make PA0 and PA1 as Digital Ports
+	MOV r0, #0x451C		;set address to 0x4000451C
+	LDR r1, [r0]
+	ORR r1, r1, 0x03
+	STR r1, [r0]
+	;Change PA0,PA1 to Use an Alternate Function
+	MOV r0, #0x4420		;set address to 0x40004420
+	LDR r1, [r0]
+	ORR r1, r1, 0x03
+	STR r1, [r0]
+	;Configure PA0 and PA1 for UART
+	MOV r0, #0x452C		;set address to 0x4000452C
+	LDR r1, [r0]
+	ORR r1, r1, #0x11
+	STR r1, [r0]
+
 		; Your code for your uart_init routine is placed here
 
 	POP {lr}
