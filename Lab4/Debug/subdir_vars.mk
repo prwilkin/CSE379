01841fd55887bee7878a55637f78a51a62ad9423
adong4 <89880104+adong4@users.noreply.github.com>
2023-03-07 13:50:24 -0500

    Add files via upload

---
+++ Lab4/Debug/subdir_vars.mk
@@ -0,0 +1,49 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+S_SRCS += \
+../lab4part1.s 
+
+C_SRCS += \
+../main.c \
+../tm4c123gh6pm_startup_ccs.c 
+
+S_DEPS += \
+./lab4part1.d 
+
+C_DEPS += \
+./main.d \
+./tm4c123gh6pm_startup_ccs.d 
+
+OBJS += \
+./lab4part1.obj \
+./main.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"lab4part1.obj" \
+"main.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"main.d" \
+"tm4c123gh6pm_startup_ccs.d" 
+
+S_DEPS__QUOTED += \
+"lab4part1.d" 
+
+S_SRCS__QUOTED += \
+"../lab4part1.s" 
+
+C_SRCS__QUOTED += \
+"../main.c" \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+
---
+++ Lab4/lab4part1.s
@@ -0,0 +1,86 @@
+	.text
+	.global lab4
+
+lab4:
+	PUSH {lr}
+	BL read_from_push_btn
+          ; Your code is placed here
+
+	POP {lr}
+	MOV pc, lr
+
+read_from_push_btn:
+	PUSH {lr}
+loop:
+	;enabling clock for Port F
+	MOV r0, #0xE000
+    MOVT r0, #0x400F
+    LDR r1, [r0, #0x608]		;load r0 to r1
+    ORR r1, r1, #0x20			;bitwise manipulation for Port F
+    STR r1, [r0, #0x608]
+    ;set GPIO Pin Direction as Input for Port F
+    MOV r0, #0x5000
+    MOVT r0, #0x4002
+    LDR r1, [r0, #0x400]
+    MVN r1, #0x10
+    AND r1, r1, #0x10
+    STR r1, [r0, #0x400]		;store 0 into GPIO Pin Direction as Input for Port F
+    ;Enable GPIO Pin 4 for Port F
+    LDR r1, [r0, #0x51C]
+	ORR r1, #0x10
+	STR r1, [r0, #0x51C]		;store 1 into Port F Pin 4
+	;Enable pull-up resistor for Port F
+	MOV r0, #0x5510
+    MOVT r0, #0x4002
+    LDR r1, [r0]
+	ORR r1, #0x10
+	STR r1, [r0]
+	;Read Port F Pin 4
+	MOV r0, #0x5000
+    MOVT r0, #0x4002
+	LDR r1, [r0, #0x3FC]
+	ORR r1, #0x10
+	CMP r1, #1
+	BEQ one						;if it 1 branch to one
+	BNE zero					;if it 0 branch to zero
+one:
+	MOV r0, #1					;set r0 as 1 (High)
+zero:
+	MOV r0, #0					;set r0 as 0 (Low)
+	B loop
+
+
+          ; Your code is placed here
+
+	POP {lr}
+	MOV pc, lr
+
+illuminate_RGB_LED:
+	PUSH {lr}
+		;Red P1 Green P3 Blue P2
+		;system clock
+		MOV r1, #0xE000		;0x400FE000
+		MOVT r1, #0x400F
+		LDR r2, [r1, #0x608]
+		ORR r2, #0x20
+		STR r2, [r1, #0x608]
+
+		;set direction
+		LDR r2, [r1, #0x400]
+		ORR r2, #0x7
+		STR r2 [r1, #0x400]
+
+		;set type (digital)
+		LDR r2, [r1, #0x51C]
+		ORR r2, #0x7
+		STR r2, [r1, #0x51C]
+
+		;led fireworks
+		STRB r0, [r1, #0x3FC]
+          ; Your code is placed here
+
+	POP {lr}
+	MOV pc, lr
+
+
+	.end
